08e2dcbb80f1513e6e98978fea13f6200eb28bbb
darthmullog <168669499+darthmullog@users.noreply.github.com>
2024-07-15 15:17:42 +0300

    Delete KATA_JDBC-master directory

--- KATA_JDBC-master/PP_1_1_4-5/KATA_JDBC-master/src/test/resources/test.sql
+++
@@ -1,5 +0,0 @@
-insert into users (name, lastName, age) values('Alex', 'Pupkin', 42);
-#select * from users ;
-#select * from users where name='Steve';
-#update users set name='Steve', age=17 where id=1;
-#delete from users where id=8;
